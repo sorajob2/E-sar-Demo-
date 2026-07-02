754f9d1a45b603fefb2bad544b1ff246c3987ca2
gs-tsu <gs-tsu@tsu.ac.th>
2026-07-02 10:51:05 +0700

    Fix evidence service authorization

--- e-sar/src/app/services/evidence.service.ts
+++ e-sar/src/app/services/evidence.service.ts
@@ -2,40 +2,49 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 
+import { HttpHeaders } from '@angular/common/http';
+
 @Injectable({
   providedIn: 'root'
 })
 export class EvidenceService {
 
+  private getHeaders() {
+    const token = localStorage.getItem('token');
+
+    return {
+      headers: new HttpHeaders({
+        Authorization: `Bearer ${token}`
+      })
+    };
+  }
+
   private apiUrl = `${environment.apiUrl}/evidence`;
 
   constructor(
     private http: HttpClient
-  ) {}
+  ) { }
 
   getByResult(resultId: number) {
-
     return this.http.get<any[]>(
-      `${this.apiUrl}/result/${resultId}`
+      `${this.apiUrl}/result/${resultId}`,
+      this.getHeaders()
     );
-
   }
 
   upload(data: FormData) {
-
     return this.http.post(
       this.apiUrl,
-      data
+      data,
+      this.getHeaders()
     );
-
   }
 
   delete(id: number) {
-
     return this.http.delete(
-      `${this.apiUrl}/${id}`
+      `${this.apiUrl}/${id}`,
+      this.getHeaders()
     );
-
   }
 
 }
